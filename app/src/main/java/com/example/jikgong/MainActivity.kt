9b4fe750db542700b5e30c5534856612c1ed9be0
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2023-12-31 00:31:24 +0900

    test
    test

--- app/src/main/java/com/example/jikgong/MainActivity.kt
+++ app/src/main/java/com/example/jikgong/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.jikgong
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import AndroidJUnitRunner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
