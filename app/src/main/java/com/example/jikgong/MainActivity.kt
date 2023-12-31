73aaf743e6bd1a18d50737027a694089048bb75b
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2023-12-31 21:08:28 +0900

    bottom_nav
    bottom_nav

--- app/src/main/java/com/example/jikgong/MainActivity.kt
+++ app/src/main/java/com/example/jikgong/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.jikgong
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import com.example.jikgong.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
