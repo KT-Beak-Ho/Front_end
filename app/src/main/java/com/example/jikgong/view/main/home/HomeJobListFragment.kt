7c4eeead04da07195af69586453393a2ddcd071a
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2024-01-08 01:00:14 +0900

    cocomiiitit

--- app/src/main/java/com/example/jikgong/view/main/home/HomeJobListFragment.kt
+++ app/src/main/java/com/example/jikgong/view/main/home/HomeJobListFragment.kt
@@ -23,10 +23,17 @@ class HomeJobListFragment : Fragment() {
         binding = FragmentHomeJoblistBinding.inflate(inflater,container,false)
         binding.test.setOnClickListener {
             val currentFragment = parentFragmentManager.findFragmentById(R.id.frame_layout)
-            
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frame_layout,HomeMapFragment())
-            fragmentTransaction.commit()
+            if(currentFragment?.javaClass?.simpleName == "HomeJobListFragment") {
+                val fragmentTransaction = parentFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.frame_layout,HomeMapFragment())
+                fragmentTransaction.commit()
+            }
+            else {
+                val fragmentTransaction = parentFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.frame_layout,HomeJobListFragment())
+                fragmentTransaction.commit()
+            }
+
 
 
         }
