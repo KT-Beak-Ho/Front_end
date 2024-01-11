9ffb17f6b98d138f38ab7ccbe7d78367eb8c1e83
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2024-01-12 00:17:21 +0900

    홈화면 작업 공고 RecyclerView 1차 추가
    근데 이게 맞는지 잘모르겠음

--- app/src/main/java/com/example/jikgong/view/main/home/HomeJobListFragment.kt
+++ app/src/main/java/com/example/jikgong/view/main/home/HomeJobListFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.jikgong.R
 import com.example.jikgong.databinding.FragmentHomeJoblistBinding
+import com.example.jikgong.view.main.adapter.JobListRVAdapter
 
 class HomeJobListFragment : Fragment() {
     private lateinit var binding: FragmentHomeJoblistBinding
+    private val jobList = ArrayList<>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {}
@@ -21,21 +23,26 @@ class HomeJobListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         // 프래그 먼트 내부에서 프래그먼트 전환하기
-        binding = FragmentHomeJoblistBinding.inflate(inflater,container,false)
+        binding = FragmentHomeJoblistBinding.inflate(inflater, container, false)
         binding.test.setOnClickListener {
             val currentFragment = parentFragmentManager.findFragmentById(R.id.frame_layout)
-            if(currentFragment == null || currentFragment?.javaClass?.simpleName == "HomeJobListFragment") {
+            if (currentFragment == null || currentFragment?.javaClass?.simpleName == "HomeJobListFragment") {
                 val fragmentTransaction = parentFragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.frame_layout,HomeMapFragment())
+                fragmentTransaction.replace(R.id.frame_layout, HomeMapFragment())
                 fragmentTransaction.commit()
-            }
-            else {
+                jobListRV()
+            } else {
                 val fragmentTransaction = parentFragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.frame_layout,HomeJobListFragment())
+                fragmentTransaction.replace(R.id.frame_layout, HomeJobListFragment())
                 fragmentTransaction.commit()
             }
         }
         return binding.root
+    }
+
+    private fun jobListRV() {
+        val jobListRVAdapter = JobListRVAdapter(requireContext(), jobList)
+//  이게 여기에 연결하는게 맞나?
     }
 
 }
